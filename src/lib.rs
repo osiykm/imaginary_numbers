f3524f91da4ab56acfd3d37ca5d6e2db3dc425d3
osiykm <osiykmukola@gmmail.com>
2021-04-05 17:48:17 +0300

    Added some fatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,51 @@
 //! # Complex numbers implementation
 //! Created for checking how traits and generics works in rust
 
+use std::ops::Add;
+use std::fmt::{Display, Formatter, Result};
 
+
+#[derive(Default, Debug, PartialEq, Copy, Clone)]
 /// A Complex number represented here
-#[derive(Default)]
-struct Complex<T> {
+pub struct Complex<T> {
     /// real part
     re: T,
     /// imaginary part
     im: T,
 }
 
-/// creating Complex number from other types
 impl<T> Complex<T> {
-    fn new (re: T, im: T) -> Self {
+    /// create Complex<T>
+    pub fn new (re: T, im: T) -> Self {
         Complex{re, im}
     }
 }
 
+
+impl<T: Add<Output = T>> Add for Complex<T> {
+    /// return complex number
+    type Output = Self;
+    /// you can complex numbers For example (x_1, y_1i) + (x_2, y_2i) = ((x_1+x_2), (y_1+y_2)i)
+    fn add(self, rhs: Self) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im
+        }
+    }
+}
+
+impl<T> From<(T, T)> for Complex<T> {
+    /// generate Complex from tulpe (T, T)
+    fn from(data: (T, T)) -> Self {
+        Complex {re: data.0, im: data.1}
+    }
+}
+
+impl<T: Display> Display for Complex<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{} + {}i", self.re, self.im)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,5 +58,20 @@ mod tests {
         assert_eq!(first.re, 3);
         assert_eq!(first.im, 5);
         assert_eq!(second.re, second.im);
+        println!("{:?}", first);
+    }
+
+    #[test]
+    fn complex_add_test() {
+        let first = Complex::new(3,5);
+        let second: Complex<i32> = Complex::default();
+        let res = first + second;
+        assert_eq!(res, first);
+    }
+
+    #[test]
+    fn complex_display() {
+        let my_imaginary = Complex::new(2345, 456);
+        println!("{}", my_imaginary);
     }
 }
