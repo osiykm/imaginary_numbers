325b96942b63582c946be0febf0429c6f53922e6
osiykm <osiykmukola@gmmail.com>
2021-04-05 17:09:13 +0300

    basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,34 @@
+//! # Complex numbers implementation
+//! Created for checking how traits and generics works in rust
+
+
+/// A Complex number represented here
+#[derive(Default)]
+struct Complex<T> {
+    /// real part
+    re: T,
+    /// imaginary part
+    im: T,
+}
+
+/// creating Complex number from other types
+impl<T> Complex<T> {
+    fn new (re: T, im: T) -> Self {
+        Complex{re, im}
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::Complex;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn complex_basics() {
+        let first = Complex::new(3,5);
+        let second: Complex<i32> = Complex::default();
+        assert_eq!(first.re, 3);
+        assert_eq!(first.im, 5);
+        assert_eq!(second.re, second.im);
     }
 }
